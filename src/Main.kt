840e62a3f7175a407359242b28d19bd04bf936d1
ankush kapoor <ankush.kapoor@paytm.com>
2019-12-20 00:23:22 +0530

    kotlin practice

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,23 @@
-fun greeting() = "Hello"
-
 fun main() {
-    print(greeting())
+    greeting()
+    kotlinCollections()
+
+    var myArray = arrayOf("Ankush", "Arsh", "Komal")
+    varargsTest("Hi", *myArray)
+}
+
+fun varargsTest(greeting: String, vararg items: String) {
+    items.forEachIndexed { index, item -> println("$greeting $item") }
+}
+
+fun greeting() = println("Hello")
+
+fun kotlinCollections() {
+    var myArray = mutableListOf("Kolin", "Java", "Python", "C++")
+    myArray.forEachIndexed { index, item ->
+        println("$index - $item")
+    }
+
+    var map = mapOf(1 to "a", 2 to "b", 3 to "c")
+    map.forEach { (key, value) -> println("$key - $value") }
 }
